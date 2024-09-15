3a276ee27fc9ef42b0b867cadfcaf0693c22a73f
ShiruvatiNarasimha <mrnattyboy05@gmail.com>
2024-09-15 21:36:26 +0530

    Add Scham

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Appbar } from "./components/Appbar";
+
 export default function Home() {
   return (
     <main>
@@ -7,3 +8,6 @@ export default function Home() {
     </main>
   );
 }
+
+console.log(process.env.GOOGLE_CLIENT_ID)
+console.log(process.env.GOOGLE_CLIENT_SECRET)
